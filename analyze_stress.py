8fd0b2b19ecfc84ef7d55e55913348435072feae
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-12-18 07:04:51 -0500

    renamed stress analysis file

--- analyze_stress.py
+++ analyze_stress.py
@@ -8,14 +8,15 @@
 #
 ##########################################################################
 
-from __future__ import print_function
-
 # require numpy, pysparse
+
+from __future__ import print_function
 
 import time as time
 
 T = int(round(time.time() * 1000))
 t0 = T
+
 
 def log(msg):
     global T
@@ -27,6 +28,9 @@ def log(msg):
 
 log(None)
 
+import argparse
+import json
+import ast
 from sys import argv
 import numpy as np
 from math import sqrt
@@ -44,25 +48,38 @@ C = np.matrix(np.load('H8.C'))
 
 log('global constant variables')
 
+
+def _save(data, name):
+    f = open(name, 'w')
+    f.write(str(data))
+    f.close()
+    print('saved to ' + name)
+
+
 def node_nums_3d(nelx, nely, nelz, mpx, mpy, mpz):
-    innback = np.array([0, 1, nely + 1, nely + 2]) #  initial node numbers at back
+    innback = np.array([0, 1, nely + 1, nely + 2])  # initial node numbers at back
     enback = nely * (mpx - 1) + mpy
     nnback = innback + enback + mpx - 1
     nnfront = nnback + (nelx + 1) * (nely + 1)
     nn = np.hstack((nnback, nnfront)) + (mpz - 1) * (nelx + 1) * (nely + 1)
     return nn
 
+
 def _sub(b, a):
-    return [b[0]-a[0], b[1]-a[1], b[2]-a[2]]
+    return [b[0] - a[0], b[1] - a[1], b[2] - a[2]]
+
 
 def _dot(u, v):
-    return u[0]*v[0] + u[1]*v[1] + u[2]*v[2]
+    return u[0] * v[0] + u[1] * v[1] + u[2] * v[2]
+
 
 def _cross(u, v):
-    return [u[1]*v[2]-u[2]*v[1], u[0]*v[2]-u[2]*v[0], u[0]*v[1]-u[1]*v[0]]
+    return [u[1] * v[2] - u[2] * v[1], u[0] * v[2] - u[2] * v[0], u[0] * v[1] - u[1] * v[0]]
+
 
 def _normsq(v):
-    return v[0]*v[0] + v[1]*v[1] + v[2]*v[2]
+    return v[0] * v[0] + v[1] * v[1] + v[2] * v[2]
+
 
 def _on_same_side(p1, p2, a, b):
     ab = _sub(b, a)
@@ -77,10 +94,12 @@ def _on_same_side(p1, p2, a, b):
 
     return sameside
 
+
 def _in_triangle(v, va, vb, vc):
     return _on_same_side(v, va, vb, vc) and \
-    _on_same_side(v, vb, va, vc) and \
-    _on_same_side(v, vc, va, vb)
+        _on_same_side(v, vb, va, vc) and \
+        _on_same_side(v, vc, va, vb)
+
 
 def _voxelize(bstl, n):
     """
@@ -102,16 +121,16 @@ def _voxelize(bstl, n):
     numTriangles = struct.unpack("@i", f.read(4))
     numTriangles = numTriangles[0]
 
-    triangles = [(0,0,0)]*numTriangles # prealloc, slightly faster than append
-    norms = [(0,0,0)]*numTriangles
-    bytecounts = [(0,0,0)]*numTriangles
+    triangles = [(0, 0, 0)] * numTriangles  # prealloc, slightly faster than append
+    norms = [(0, 0, 0)] * numTriangles
+    bytecounts = [(0, 0, 0)] * numTriangles
 
     for i in xrange(numTriangles):
         norms[i] = struct.unpack("<3f", f.read(12))
         vertex1 = struct.unpack("<3f", f.read(12))
         vertex2 = struct.unpack("<3f", f.read(12))
         vertex3 = struct.unpack("<3f", f.read(12))
-        bytecounts[i] = struct.unpack("H", f.read(2)) # not sure what this is
+        bytecounts[i] = struct.unpack("H", f.read(2))  # not sure what this is
 
         triangles[i] = (vertex1, vertex2, vertex3)
 
@@ -126,17 +145,16 @@ def _voxelize(bstl, n):
             for i in range(0, 3):
                 vmin[i] = min(v[i], vmin[i])
                 vmax[i] = max(v[i], vmax[i])
-                ctr[i] += v[i]/3
+                ctr[i] += v[i] / 3
         centers.append(ctr)
 
-    dim = max(vmax[0]-vmin[0], vmax[1]-vmin[1], vmax[2]-vmin[2]) / n
-    nx = int((vmax[0]-vmin[0]) / dim + 0.5)
-    ny = int((vmax[1]-vmin[1]) / dim + 0.5)
-    nz = int((vmax[2]-vmin[2]) / dim + 0.5)
+    dim = max(vmax[0] - vmin[0], vmax[1] - vmin[1], vmax[2] - vmin[2]) / n
+    nx = int((vmax[0] - vmin[0]) / dim + 0.5)
+    ny = int((vmax[1] - vmin[1]) / dim + 0.5)
+    nz = int((vmax[2] - vmin[2]) / dim + 0.5)
 
     log('done')
     print('num of triangles', len(triangles))
-    print('voxel grid: ' + str(nx) + ' x ' + str(ny) + ' x ' + str(nz))
 
     vxg = []
 
@@ -148,33 +166,33 @@ def _voxelize(bstl, n):
         for j in range(0, ny):
             vxgrow = []
             for k in range(0, nx):
-                perc = cntr*100.0/(nx*ny*nz)
+                perc = cntr * 100.0 / (nx * ny * nz)
                 print('voxelizing ...... ' + str(int(perc)) + '%', end='\r')
                 # print('voxelizing ...... ' + str(int(perc)) + '%', end='\r')
 
-                ctrvoxel = [(k+0.5)*dim, (j+0.5)*dim, (i+0.5)*dim]
-                counter = [[0,0], [0,0], [0,0]]
+                ctrvoxel = [(k + 0.5) * dim, (j + 0.5) * dim, (i + 0.5) * dim]
+                counter = [[0, 0], [0, 0], [0, 0]]
                 for h in range(0, len(triangles)):
                     t = triangles[h]
                     ctrface = centers[h]
                     for l in range(0, NUMDIR):
                         projctrvoxel = list(ctrvoxel)
                         projva, projvb, projvc = list(t[0]), list(t[1]), list(t[2])
-                        if l==0:
+                        if l == 0:
                             projctrvoxel[0] = 0
                             projva[0] = 0
                             projvb[0] = 0
                             projvc[0] = 0
                             if _in_triangle(projctrvoxel, projva, projvb, projvc):
                                 counter[l][0 if ctrvoxel[0] < ctrface[0] else 1] += 1
-                        elif l==1:
+                        elif l == 1:
                             projctrvoxel[1] = 0
                             projva[1] = 0
                             projvb[1] = 0
                             projvc[1] = 0
                             if _in_triangle(projctrvoxel, projva, projvb, projvc):
                                 counter[l][0 if ctrvoxel[1] < ctrface[1] else 1] += 1
-                        elif l==2:
+                        elif l == 2:
                             projctrvoxel[2] = 0
                             projva[2] = 0
                             projvb[2] = 0
@@ -184,68 +202,58 @@ def _voxelize(bstl, n):
 
                 isvoxel = False
                 for l in range(0, NUMDIR):
-                    if counter[l][0]%2 == 1 and counter[l][1]%2 == 1:
+                    if counter[l][0] % 2 == 1 and counter[l][1] % 2 == 1:
                         isvoxel = True
                         break
                 vxgrow.append(1 if isvoxel else 0)
-                cntr+=1
+                cntr += 1
             vxgplane.append(vxgrow)
         vxg.append(vxgplane)
 
     print('')
     log('done')
-    return vxg
+    return {'dimvoxel': dim, 'voxelgrid': vxg}
+
 
 def _load_vxg(vxgpath):
     vxgraw = open(vxgpath, 'r').read()
     idxdim = vxgraw.index('\n')
     dim = float(vxgraw[:idxdim])
-    vxgraw = vxgraw[idxdim+1:]
-
-    vxg = []
-    vxgplanes = vxgraw.split('\n\n')
-    for plane in vxgplanes:
-        vxgplane = []
-        planeraw = plane.split('\n')
-        for row in planeraw:
-            vxgrow = []
-            rowraw = row.split(',')
-            for vx in rowraw:
-                try:
-                    vxgrow.append(int(vx))
-                except:
-                    continue
-            if len(vxgrow) > 0:
-                vxgplane.append(vxgrow)
-        vxg.append(vxgplane)
+    vxgraw = vxgraw[idxdim + 1:]
+    vxg = ast.literal_eval(vxgraw)
     return vxg
 
-# input:
-#   - vxgpath: path to voxel grid file
-#   - loads: each consisting of
-#       * points
-#       * value
-#   - boundary
-#       * points
-#
-# output:
-#   - displacement vector
-def analyze(path, n, loads, boundary, iter):
+
+def analyze(vxg, loads, boundary, iter):
+    '''
+    main analysis function
+       - vxg: voxel grid (3d list)
+       - loads: each consisting of
+           * points
+           * value
+       - boundary
+           * points
+
+    output:
+       - displacement vector
+       - von Mises stress vector
+    '''
     global Ke, B, C
 
-    print('mesh or voxel grid path: ' + path)
-
-    # load voxel grid
-    # vxg = _load_vxg(vxgpath)
-    vxg = _voxelize(path, n)
+    # print('mesh or voxel grid path: ' + path)
+    #
+    # # load voxel grid
+    # # vxg = _load_vxg(vxgpath)
+    # vxg = _voxelize(path, n)
     nz = len(vxg)
     ny = len(vxg[0])
     nx = len(vxg[0][0])
     log('voxelization')
+    print('voxel grid: ' + str(nx) + ' x ' + str(ny) + ' x ' + str(nz))
 
     # compute stiffness matrix for individual elements
     DOF = 3
-    ksize = DOF * (nx+1) * (ny+1) * (nz+1)
+    ksize = DOF * (nx + 1) * (ny + 1) * (nz + 1)
     kall = spmatrix.ll_mat_sym(ksize, ksize)
     # print kall
     # for i in range(0, ksize):
@@ -257,10 +265,10 @@ def analyze(path, n, loads, boundary, iter):
         for j in range(0, ny):
             for k in range(0, nx):
                 xe = SOLID if vxg[i][j][k] == 1 else VOID
-                nodes = node_nums_3d(nx, ny, nz, k+1, j+1, i+1)
+                nodes = node_nums_3d(nx, ny, nz, k + 1, j + 1, i + 1)
                 ind = []
                 for n in nodes:
-                    ind.extend([(n-1)*DOF, (n-1)*DOF+1, (n-1)*DOF+2])
+                    ind.extend([(n - 1) * DOF, (n - 1) * DOF + 1, (n - 1) * DOF + 2])
                 mask = np.ones(len(ind), dtype=int)
                 kall.update_add_mask_sym(Ke * xe, ind, mask)
 
@@ -268,19 +276,22 @@ def analyze(path, n, loads, boundary, iter):
 
     # formulate loading scenario
     rall = [0] * ksize
-    indices = loads['points']
-    value = loads['value']
+    indicesset = loads['points']
+    values = loads['values']
 
-    for idx in indices:
-        nodes = node_nums_3d(nx, ny, nz, idx[0]+1, idx[1]+1, idx[2]+1)
-        for j in range(0, DOF):
-            for k in range(0, len(nodes)):
-                rall[DOF * (nodes[k]-1) + j] = value[j]
+    for i in range(0, len(indicesset)):
+        indices = indicesset[i]
+        value = values[i]
+        for idx in indices:
+            nodes = node_nums_3d(nx, ny, nz, idx[0] + 1, idx[1] + 1, idx[2] + 1)
+            for j in range(0, DOF):
+                for k in range(0, len(nodes)):
+                    rall[DOF * (nodes[k] - 1) + j] = value[j]
 
     # formulate boundary condition
-    elemmask = [1] * (nx+1) * (ny+1) * (nz+1)
+    elemmask = [1] * (nx + 1) * (ny + 1) * (nz + 1)
     for idx in boundary:
-        nodes = node_nums_3d(nx, ny, nz, idx[0]+1, idx[1]+1, idx[2]+1)
+        nodes = node_nums_3d(nx, ny, nz, idx[0] + 1, idx[1] + 1, idx[2] + 1)
         for j in range(0, len(nodes)):
             elemmask[nodes[j] - 1] = 0
 
@@ -288,9 +299,9 @@ def analyze(path, n, loads, boundary, iter):
     fixeddofs = []
     for i in range(0, len(elemmask)):
         if elemmask[i] == 1:
-            freedofs.extend((DOF*i, DOF*i+1, DOF*i+2))
+            freedofs.extend((DOF * i, DOF * i + 1, DOF * i + 2))
         else:
-            fixeddofs.extend((DOF*i, DOF*i+1, DOF*i+2))
+            fixeddofs.extend((DOF * i, DOF * i + 1, DOF * i + 2))
 
     log('formulated loading scenario and boundary condition')
 
@@ -320,24 +331,25 @@ def analyze(path, n, loads, boundary, iter):
 
     log('solved KU=F')
 
-    dall = np.zeros_like(rall);
+    dall = np.zeros_like(rall)
     for i in range(0, len(freedofs)):
         dall[freedofs[i]] = dfree[i]
 
     # compute stress
-    cb = C*B
+    cb = C * B
     vonmises = []
     for i in range(0, nz):
         vmplane = []
         for j in range(0, ny):
             vmrow = []
             for k in range(0, nx):
-                nodes = node_nums_3d(nx, ny, nz, k+1, j+1, i+1)
+                nodes = node_nums_3d(nx, ny, nz, k + 1, j + 1, i + 1)
                 disps = []
                 for n in nodes:
-                    disps.extend([dall[DOF*(n-1)], dall[DOF*(n-1)+1], dall[DOF*(n-1)+2]])
+                    disps.extend([dall[DOF * (n - 1)], dall[DOF * (n - 1) + 1],
+                                  dall[DOF * (n - 1) + 2]])
                 d = np.matrix(disps).transpose()
-                sigma = cb*d
+                sigma = cb * d
                 # sigma = np.array(sigma).astype('float')
                 # sigmas.append([x[0] for x in sigma])
                 s11 = sigma.item(0, 0)
@@ -348,35 +360,56 @@ def analyze(path, n, loads, boundary, iter):
                 s31 = sigma.item(5, 0) * 0.5
 
                 # von Mises stress
-                vmrow.append(sqrt(0.5*((s11-s22)**2 + (s22-s33)**2 + (s33-s11)**2 +\
-                 6*(s12**2 + s23**2 + s31**2))))
+                vmrow.append(sqrt(0.5 * ((s11 - s22)**2 + (s22 - s33)**2 + (s33 - s11)**2 +
+                                         6 * (s12**2 + s23**2 + s31**2))))
             vmplane.append(vmrow)
         vonmises.append(vmplane)
-
 
     t1 = log('computed stress')
 
     global t0
-    print('total time:' + str(t1-t0) + ' ms')
+    print('total time:' + str(t1 - t0) + ' ms')
 
-    return {'disp': dall.tolist(), 'stress': vonmises}
+    return {'displacements': dall.tolist(), 'stress': vonmises}
 
 #
 #   main function entry point
 #
 if __name__ == "__main__":
-    # result = analyze(test.MESHPATH2, 64, test.LOAD2, test.BC2, True)
-    # result = analyze(VXGPATH1, LOAD1, BC1, True)
+    # setting up parameters
+    parser = argparse.ArgumentParser(description='stress analysis')
+    parser.add_argument('path', metavar='path', type=str,
+                        help='path to a .stl (mesh) or .vxg (voxel grid) file')
+    parser.add_argument('-d', dest='dimension', nargs='?', default=32,
+                        help='dimension of the voxel grid (if want to perform voxelization, default is 32)')
+    parser.add_argument('-c', dest='config',
+                        help='path to a configuration file (.json) describing loading scenario and boundary condition')
+    parser.add_argument('-o', dest='output', nargs='?', default='result',
+                        help='output path for analysis results: displacements and stress')
+    args = parser.parse_args()
 
-    _voxelize(test.MESHPATH3, 64)
+    # parsing configuration
+    config = json.loads(open(args.config, 'r').read())
+    boundary = config['boundary']
+    loads = {'points': config['loadpoints'], 'values': config['loadvalues']}
 
-    # disp = result['disp']
-    # f = open(str(long(time.time())) + '.disp', 'w')
-    # f.write(str(disp))
-    # f.close()
+    # creating or loading voxel grid
+    vxg = None
+    if args.path.endswith('.stl'):
+        vxginfo = _voxelize(args.path, int(args.dimension))
+        vxg = vxginfo['voxelgrid']
+        _save(str(vxginfo['dimvoxel']) + '\n' + str(vxg),
+              args.path.split('.')[0] + '_' + str(args.dimension) + '.vxg')
+    elif args.path.endswith('.vxg'):
+        vxg = _load_vxg(args.path)
+    else:
+        raise TypeError('must be .stl (mesh) or .vxg (voxel grid) file')
 
-    # stress = result['stress']
-    # f = open(str(long(time.time())) + '.strs', 'w')
-    # f.write(str(stress))
-    # f.close()
-    # print np.array([0, 2, 1]) + np.array([2, 3, 1])
+    # performing analysis
+    if vxg != None:
+        result = analyze(vxg, loads, boundary, True)
+        if args.output != None:
+            _save(result['displacements'], args.output + '.disp')
+            _save(result['stress'], args.output + '.strs')
+    else:
+        raise ValueError('cannot create or load voxel grid')
