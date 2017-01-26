992f3fd09100ed00e13ec8fdc7f0d15c2f91b54f
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2017-01-26 15:59:56 -0500

    updated batch

--- batch_voxelize.py
+++ batch_voxelize.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     dim = 64
 
     objs = []
-    objs.append('teapot')
-    objs.append('curtain_hook')
-    objs.append('breakable_a_v2')
-    objs.append('breakable_b_v2')
-    objs.append('pikachu')
+    # objs.append('teapot')
+    # objs.append('curtain_hook')m ,
+    # objs.append('breakable_a_v2')
+    # objs.append('breakable_b_v2')
+    # objs.append('pikachu')
     objs.append('bunny')
     objs.append('brackets')  # http://www.thingiverse.com/thing:1538314
     objs.append('bagholder')  # http://www.thingiverse.com/thing:26767
@@ -31,7 +31,6 @@ if __name__ == "__main__":
             vxg = vxginfo['voxelgrid']
             _save(str(vxginfo['dimvoxel']) + '\n' + str(vxg),
                   obj + '_' + str(dim) + '.vxg')
-            break
         except:
             print 'an error occurred!'
             continue
